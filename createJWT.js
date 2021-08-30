36b50295c260db537ee44eb77ede7c573438e89f
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-08-30 21:12:39 +0000

    refactor: use try-catch

--- createJWT.js
+++ createJWT.js
@@ -4,20 +4,28 @@ require('dotenv').config();
 let duration = 25 * 365.25 * 24 * 60 * 60;
 const secret = process.env.JWT_SECRET;
 
-// Generate payload
-let token = jwt.sign({ 
-	iss: 'jwt-tutorial', // Issuer (who created and signed this token)
-	sub: 'USER_ID', // Subject (whom the token refers to)
-	exp: Math.floor(Date.now() / 1000) + duration, // Token expiry date
-	roles: ['student']
-}, secret);
+try {
+	// Generate payload
+	let token = jwt.sign({ 
+		iss: 'jwt-tutorial', // Issuer (who created and signed this token)
+		sub: 'USER_ID', // Subject (whom the token refers to)
+		exp: Math.floor(Date.now() / 1000) + duration, // Token expiry date
+		roles: ['student']
+	}, secret);
 
-console.log("-- Generated JWT --");
-console.log('JWT :', token);
+	console.log("-- Generated JWT --");
+	console.log('JWT :', token);
+} catch (errorOnPayload) {
+	console.log("Failed to generate payload! An error happened :", errorOnPayload);
+}
 
-let decoded = jwt.decode(token, { complete: true });
+try {
+	let decoded = jwt.decode(token, { complete: true });
 
-console.log("\n-- Decoded JWT --");
-console.log("Header :", decoded.header);
-console.log("Payload :", decoded.payload);
-console.log("Signature :", decoded.signature); // Header + Payload generate signature
+	console.log("\n-- Decoded JWT --");
+	console.log("Header :", decoded.header);
+	console.log("Payload :", decoded.payload);
+	console.log("Signature :", decoded.signature); // Header + Payload generate signature
+} catch (errorOnDecode) {
+	console.log("Failed to decode JWT! An error occurred :", errorOnDecode);
+}
