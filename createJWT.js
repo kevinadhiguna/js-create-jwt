6b7ce44543a0e777d1df13f5d307e4730e6653f1
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-08-30 19:49:05 +0000

    refactor: use environment variables

--- createJWT.js
+++ createJWT.js
@@ -1,12 +1,14 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 let duration = 25 * 365.25 * 24 * 60 * 60;
+const secret = process.env.JWT_SECRET;
 
 let token = jwt.sign({ 
 	iss: 'jwt-tutorial', 
 	sub: 'USER_ID', 
 	exp: Math.floor(Date.now() / 1000) + duration,
 	roles: ['student']
-}, 'cb098a48b008625b7e829ffd2f301f669a1a6873c58a7ad9bd18fb6e5280bbb2');
+}, secret);
 
 console.log('JWT :', token);
