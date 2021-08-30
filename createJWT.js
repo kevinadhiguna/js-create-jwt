af483979b12d3984bc5dbf3010b8811cbc0cf407
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-08-30 19:56:18 +0000

    chore: add description in generating payload

--- createJWT.js
+++ createJWT.js
@@ -4,10 +4,11 @@ require('dotenv').config();
 let duration = 25 * 365.25 * 24 * 60 * 60;
 const secret = process.env.JWT_SECRET;
 
+// Generate payload
 let token = jwt.sign({ 
-	iss: 'jwt-tutorial', 
-	sub: 'USER_ID', 
-	exp: Math.floor(Date.now() / 1000) + duration,
+	iss: 'jwt-tutorial', // Issuer (who created and signed this token)
+	sub: 'USER_ID', // Subject (whom the token refers to)
+	exp: Math.floor(Date.now() / 1000) + duration, // Token expiry date
 	roles: ['student']
 }, secret);
 
